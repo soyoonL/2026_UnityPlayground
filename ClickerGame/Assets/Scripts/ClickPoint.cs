cdb2a4c01230c0611ffaa084ad647c2bf9b3e00e
soyoonL <diamomo777@naver.com>
2026-08-03 11:06:26 +0900

    [feat] Upgrade 할 때마다 point 차감

--- ClickerGame/Assets/Scripts/ClickPoint.cs
+++ ClickerGame/Assets/Scripts/ClickPoint.cs
@@ -3,27 +3,35 @@ using UnityEngine.UI;
 
 public class ClickPoint : MonoBehaviour
 {
-    [SerializeField]
-    private Image characterImage;
+    [Header("업그레이드 시 이미지 변경")]
+    public Image characterImage;
     public Sprite[] evolutionSprites;
     private int evolutionStage = 0;
 
-    public int point = 0;
+    [Header("포인트")]
+    public int currentPoint = 0;
     public int pointPerClick = 1;
+    public int requiredPoint=5;
 
     public void PointUp()
     {
-        point+= pointPerClick;
-        Debug.Log(point);
+        currentPoint+= pointPerClick;
+        Debug.Log(currentPoint);
     }
 
     public void Upgrade()
     {
-        if (evolutionStage < evolutionSprites.Length )
+        if (evolutionStage < evolutionSprites.Length && currentPoint >= requiredPoint)
         {
             characterImage.sprite = evolutionSprites[evolutionStage];
             evolutionStage++;
-            pointPerClick = pointPerClick * 2;
+            currentPoint -= requiredPoint;
+            requiredPoint *= 3;
+            pointPerClick *= 2;
+        }
+        else if(currentPoint < requiredPoint)
+        {
+            Debug.Log("포인트가 부족해용..");
         }
         else
         {
