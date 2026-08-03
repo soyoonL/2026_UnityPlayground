7e251dba98abe6089eb949296066ea96af80e35c
soyoonL <diamomo777@naver.com>
2026-08-03 10:35:05 +0900

    [feat] Upgrade 버튼 클릭 시 Charater 버튼 Sprite 변경 및 클릭 포인트 2배 증가

--- ClickerGame/Assets/Scripts/ClickPoint.cs
+++ ClickerGame/Assets/Scripts/ClickPoint.cs
@@ -1,13 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClickPoint : MonoBehaviour
 {
+    [SerializeField]
+    private Image characterImage;
+    public Sprite[] evolutionSprites;
+    private int evolutionStage = 0;
+
     public int point = 0;
-   
+    public int pointPerClick = 1;
+
     public void PointUp()
     {
-        point++;
+        point+= pointPerClick;
         Debug.Log(point);
+    }
+
+    public void Upgrade()
+    {
+        if (evolutionStage < evolutionSprites.Length )
+        {
+            characterImage.sprite = evolutionSprites[evolutionStage];
+            evolutionStage++;
+            pointPerClick = pointPerClick * 2;
+        }
+        else
+        {
+            Debug.Log("³¡!");
+        }
+
     }
 
    
