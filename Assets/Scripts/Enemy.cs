fff9d25c36e2ebfe9cf5ef3bc410e071d8bac331
soyoonL <diamomo777@naver.com>
2026-08-10 19:25:48 +0900

    [feat] 임시 적 Hp 슬라이더 추가

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameManager gameManager;
+    [SerializeField] Slider hpSlider;
     public float EnemyHp = 100;
     public int EnemyKillPoint = 10;
+
+    void Start()
+    {
+        hpSlider.maxValue = EnemyHp;
+        hpSlider.value = EnemyHp;
+    }
 
     public void TakeDamage(int damage)
     {
         EnemyHp-=damage;
-        
-        if(EnemyHp <= 0)
+        hpSlider.value = EnemyHp;
+
+        if (EnemyHp <= 0)
         {
             gameManager.PointUp();
             Destroy(gameObject);
+            hpSlider.gameObject.SetActive(false);
         }
     }
 }
