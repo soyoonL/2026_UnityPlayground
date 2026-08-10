d5aab410e187fbaa0a2d5708dd7d6070480392e2
soyoonL <diamomo777@naver.com>
2026-08-10 14:24:29 +0900

    [feat] Text가 커졌다가 작아지는 효과 추가

--- Assets/Scripts/ClickPoint.cs
+++ Assets/Scripts/ClickPoint.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,34 +13,96 @@ public class ClickPoint : MonoBehaviour
     [Header("포인트")]
     public int currentPoint = 0;
     public int pointPerClick = 1;
-    public int requiredPoint=5;
+    public int requiredPoint= 5;
+
+    [Header("텍스트")]
+    public TextMeshProUGUI evolutionPointCountText;
+    public TextMeshProUGUI pointCountText;
+
+    Coroutine textEffectCoroutine;
+    Vector3 ogPointTextScale;
+
+    private void Start()
+    {
+        ogPointTextScale = pointCountText.transform.localScale;  // 원래 텍스트의 크기를 담아두는 변수
+        ResetText();
+    }
 
     public void PointUp()
     {
         currentPoint+= pointPerClick;
         Debug.Log(currentPoint);
+
+        ResetText();
+        DoPointTextEffect(Color.gold, 1.2f);
     }
 
     public void Upgrade()
     {
         if (evolutionStage < evolutionSprites.Length && currentPoint >= requiredPoint)
         {
-            characterImage.sprite = evolutionSprites[evolutionStage];
-            evolutionStage++;
-            currentPoint -= requiredPoint;
-            requiredPoint *= 3;
-            pointPerClick *= 2;
+            Evolution();
+            ResetText();
+            DoPointTextEffect(Color.red, 0.8f);
+            if (evolutionStage == evolutionSprites.Length)
+            {
+                evolutionPointCountText.text = "Evolution End!";
+            }
         }
-        else if(currentPoint < requiredPoint)
-        {
-            Debug.Log("포인트가 부족해용..");
-        }
-        else
-        {
-            Debug.Log("끝!");
-        }
+       
 
     }
 
-   
+    void Evolution()
+    {
+        characterImage.sprite = evolutionSprites[evolutionStage];
+        evolutionStage++;
+        currentPoint -= requiredPoint;
+        requiredPoint *= 3;
+        pointPerClick *= 2;
+    }
+
+    void ResetText()
+    {
+        pointCountText.text = currentPoint.ToString() + "Points";
+
+        if(evolutionStage != evolutionSprites.Length)
+        {
+            evolutionPointCountText.text = "Need " + requiredPoint.ToString() + "Points";
+        }
+    }
+
+    void DoPointTextEffect(Color col,float scale)
+    {
+        if(textEffectCoroutine != null) StopCoroutine(textEffectCoroutine);
+        textEffectCoroutine = StartCoroutine(TextEffect(pointCountText,0.15f,col,scale));
+    }
+
+    IEnumerator TextEffect(TextMeshProUGUI go, float duration, Color col, float scale)
+    {
+        Vector3 targetScale = ogPointTextScale * scale;
+        Color ogColor = go.color;
+        duration /= 2;
+
+        float t = 0;
+        while(t < duration)
+        {
+            t +=Time.deltaTime;
+            go.transform.localScale = Vector3.Lerp(ogPointTextScale, targetScale, t / duration);
+            go.color = Color.Lerp(ogColor, col, t / duration); ;
+            yield return null;
+        }
+        t = 0;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            go.transform.localScale = Vector3.Lerp(targetScale, ogPointTextScale, t / duration);
+            go.color = Color.Lerp(col, ogColor, t / duration);
+            yield return null;
+        }
+
+        go.transform.localScale = ogPointTextScale;
+        go.color = ogColor;
+        textEffectCoroutine = null;
+    }
 }
