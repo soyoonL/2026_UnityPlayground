b75826015f8f1e95efb742ee0d2887ecb56e79a5
soyoonL <diamomo777@naver.com>
2026-08-20 01:21:13 +0900

    [docs] 오브젝트 풀링 방식 설명을 위한 주석 추가

--- Assets/Scripts/ClickShooter.cs
+++ Assets/Scripts/ClickShooter.cs
@@ -7,21 +7,23 @@ public class ClickShooter : MonoBehaviour
     [SerializeField] private Transform spawnPoint;   // 소환되는 위치
     [SerializeField] private Transform targetEnemy; // 적의 위치가 고정되어있는 상태이므로 인스펙터에서 끌어오는 형식으로 ㄱ
 
-    private IObjectPool<Projectile> projectilePool; // 선언
+    private IObjectPool<Projectile> projectilePool; // 발사체를 관라힐 객체 풀 변수 선언
 
     private void Awake()
     {
+        // 유니티 내장 객체 풀 초기화
         projectilePool = new ObjectPool<Projectile>(
             createFunc: CreatProjectile,
             actionOnGet: OnGetProjectile,
             actionOnRelease: OnReleaseProjectile,
             actionOnDestroy: OnDestroyProjectile,
-            collectionCheck: true,
-            defaultCapacity: 10,
-            maxSize: 50
+            collectionCheck: true, // 중복 반환 검사 (실수로 코드 작성을 잘못해서 같은 오브젝트를 두 번 반환하는 버그 발생 시 콘솔 창에 에러를 띄워 알려줌)
+            defaultCapacity: 10, // 기본 생성 수량 (pool안의 내부 배열의 크기를 미리 할당, 미리 할당 안해두면 메모리를 재할당하는 과정에서 렉 발생
+            maxSize: 50 // 내부 배열에 보관할 최대 수량
             );
     }
 
+    /// <summary> 풀 내부 배열에 재사용 가능한 발사체가 부족할 때 호출되어 새 발사체를 생성, pool이 비어있는 경우 Get() 실행 시 CreateProjectile() 먼저 호출 </summary>
     private Projectile CreatProjectile()
     {
         Projectile bullet = Instantiate(projectilePrefab);
@@ -29,31 +31,35 @@ public class ClickShooter : MonoBehaviour
         return bullet;
     }
 
+    /// <summary> Get() 호출 시 실행되는 함수로, 발사체 오브젝트를 활성화  </summary>
     private void OnGetProjectile(Projectile bullet)
     {
         bullet.gameObject.SetActive(true);
     }
 
+    /// <summary> Release() 호출 시 실행되는 함수로, 발사체 오브젝트를 비활성화, 처음 비활성화된 오브젝트는 pool 내부 배열 안에 저장됨 </summary>
     private void OnReleaseProjectile(Projectile bullet)
     {
         bullet.gameObject.SetActive(false);
     }
 
+    /// <summary> maxSize 초과 같이 오브젝트 완전 파괴가 필요할 때 실행 </summary>
     private void OnDestroyProjectile(Projectile bullet)
     {
         Destroy(bullet.gameObject);
     }
 
+    /// <summary> 캐릭터 클릭 시 발사체를 발사 </summary>
     public void Shoot()
     {
-        if(targetEnemy == null) return;
+        if(targetEnemy == null) return; // 적이 없을 경우 반환
 
-        Projectile bullet = projectilePool.Get();
+        Projectile bullet = projectilePool.Get(); 
 
-        bullet.transform.position = spawnPoint.position;
+        bullet.transform.position = spawnPoint.position; // 발사체가 생성되는 위치
 
-        int currentDamage = GameManager.Instance.CurrentDamage;
-        bullet.SetTarget(targetEnemy,currentDamage);
+        int currentDamage = GameManager.Instance.CurrentDamage; // 발사체의 데미지
+        bullet.SetTarget(targetEnemy,currentDamage); 
     }
 
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,11 +4,12 @@ using UnityEngine.Pool;
 
 public class Projectile : MonoBehaviour
 {
-    private IObjectPool<Projectile> managedPool; 
+    private IObjectPool<Projectile> managedPool; // pool 객체(관리자 시스템)의 주소를 담은 변수
     private Transform target; // 적의 위치를 담을 변수
     private float speed = 10f; // 발사체의 속도
     public int damage; // 발사체의 데미지
 
+    /// <summary> 발사체가 반납될 객체 풀의 참조를 등록 </summary>
     public void SetPool(IObjectPool<Projectile> pool)
     {
         managedPool = pool;
@@ -39,7 +40,7 @@ public class Projectile : MonoBehaviour
     }
 
     /// <summary>
-    /// 적과 충돌 시 데미지를 입히고 발사체를 파괴(유니티 이벤트 함수)
+    /// 적과 충돌 시 데미지를 입히고 발사체를 비활성화(유니티 이벤트 함수)
     /// </summary>
     /// <param name="other">충돌한 적의 Collider2D</param>
     private void OnTriggerEnter2D(Collider2D other)
@@ -53,15 +54,16 @@ public class Projectile : MonoBehaviour
         }
     }
 
+    /// <summary> 발사체를 풀에 반환(비활성화)하거나, 풀이 없는 경우 오브젝트를 파괴 </summary>
     private void ReleaseToPool()
     {
         if(managedPool != null)
         {
-            managedPool.Release(this);
+            managedPool.Release(this); // 스스로를 비활성화하고 반납
         }
         else
         {
-            Destroy(gameObject);
+            Destroy(gameObject); // managedPool이 null인 상태에서 managedPool.Release(this);를 호출하면 에러가 발생할 수 있어서 이를 대비한 안전장치
         }
     }
 }
